b2b21f328b5312b6e4d3f1c19e7db9ae9000aaab
Lana Prijon <lana.prijon@gmail.com>
2022-11-04 14:49:52 +0100

    html files saved

--- collect_data.py
+++ collect_data.py
@@ -13,7 +13,7 @@ LOGGER = logging.getLogger()
 logging.basicConfig(
     format = "%(asctime)s %(name)-12s %(levelname)-8s %(message)s", level = logging.INFO)
 
-def page_url(n):
+def find_page_url(n):
     '''
     Summary
     ----------
@@ -78,14 +78,58 @@ def add_to_df(partial_df):
     ----------
     '''
     
-def nekineki():
-    re.compile(
-        r"productListItem\" aria-label='(?P<title>.*?)'>"
-        r'bc-size-medium\" >\s*<a class=\"bc-link\s*bc-color-link\" tabindex=\"0\"  href=\"(?P<audiobook_link>.*?)\">'
-        r'Length:(?P<length>.*?)</span>' #2times per audiobook
-        r'Release date:(?P<releade_date>.*?)</span>'
-        r'bc-color-base\"  >Regular price: </span>\s*<span class=\"bc-text\s*bc-size-base \s*bc-color-base\"  >\s*(?P<price>.*?)\s'
-               )
+def nekineki(file_name):
+    with open(file_name, encoding='utf-8') as f:
+        html =  f.read()
+    
+    block_sample = re.compile(
+        r'productListItem\" aria-label=.*?'
+        r'Free with 30-day trial',
+        flags=re.DOTALL
+    )
+    #audiobook_sample = re.compile(
+    #    r"productListItem\" aria-label='(?P<title>.*?)'>"
+    #    r'bc-size-medium\" >\s*<a class=\"bc-link\s*bc-color-link\" tabindex=\"0\"  href=\"(?P<audiobook_link>.*?)\">'
+    #    r'Release date:(?P<releade_date>.*?)</span>'
+    #    r'bc-color-base\"  >Regular price: </span>\s*<span class=\"bc-text\s*bc-size-base \s*bc-color-base\"  >\s*(?P<price>.*?)\s',
+    #    flags=re.DOTALL
+    #)
+    title_sample = re.compile(
+        r"productListItem\" aria-label='(?P<title>.*?)'>", 
+        flags=re.DOTALL
+    )
+    audiobook_link_sample = re.compile(
+        r'bc-size-medium\" >\s*<a class=\"bc-link\s*bc-color-link\" tabindex=\"0\"  href=\"(?P<audiobook_link>.*?)\">', 
+        flags=re.DOTALL
+    )
+    release_date_sample = re.compile(
+        r'Release date:(?P<releade_date>.*?)</span>', 
+        flags=re.DOTALL
+    )
+    price_sample = re.compile(
+        r'bc-color-base\"  >Regular price: </span>\s*<span class=\"bc-text\s*bc-size-base \s*bc-color-base\"  >\s*(?P<price>.*?)\s', 
+        flags=re.DOTALL
+    )
+    length_sample = re.compile(
+        r'Length:(?P<length>.*?)</span>', #2times per audiobook
+        flags=re.DOTALL
+    )
+
+    for block in block_sample.finditer(html):
+        #audiobook = audiobook_sample.search(block.group(0)).groupdict()
+        title = title_sample.search(block.group(0))
+        audiobook_link = audiobook_link_sample.search(block.group(0))
+        release_date = release_date_sample.search(block.group(0))
+        price = price_sample.search(block.group(0))
+        length = length_sample.search(block.group(0))
+        #print([length, title, audiobook_link, release_date, price])
+        print(audiobook_link)
+
+#nekineki('html_files/first_page.html')
+def save_all_html():
+    for n in range(24):
+        page_url = find_page_url(n+1)
+        save_html(page_url, f'page_{n+1}.html')
 
 def main():
     '''
@@ -100,11 +144,10 @@ def main():
     '''
     num_pages = 20
     df =  pd.DataFrame(data={'Title':[], 'Author':[], 'Narrator':[], 'Release':[], 'Length':[], 'Price':[], 'Rating':[], 'Categories':[]})
-    for n in range(num_pages):
-        url = page_url(n+1)
-        save_html(url, f'page_{n+1}.html')
     for html_file in os.listdir('html_files'):
         html_filename = '...'
         partial_df = create_partial_df(html_filename)
         add_to_df(partial_df)
     return df
+
+save_all_html()
