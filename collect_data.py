9acc18e9cb6583dbde64538361a205e190b6d25d
Lana Prijon <lana.prijon@gmail.com>
2022-11-04 21:35:37 +0100

    collecting data from htmls

--- collect_data.py
+++ collect_data.py
@@ -78,8 +78,27 @@ def add_to_df(partial_df):
     ----------
     '''
     
-def nekineki(file_name):
-    with open(file_name, encoding='utf-8') as f:
+def write_csv(dictionaries, field_names, file_name):
+    '''
+    Summary
+    ----------
+
+    Parameters
+    ----------
+
+    Returns
+    ----------
+    '''
+    location = os.path.dirname(file_name)
+    if location:
+        os.makedirs(location, exist_ok=True)
+    with open(file_name, 'w', encoding='utf-8') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(dictionaries)
+
+def read_from_html(file_path):
+    with open(file_path, encoding='utf-8') as f:
         html =  f.read()
     
     block_sample = re.compile(
@@ -87,45 +106,24 @@ def nekineki(file_name):
         r'Free with 30-day trial',
         flags=re.DOTALL
     )
-    #audiobook_sample = re.compile(
-    #    r"productListItem\" aria-label='(?P<title>.*?)'>"
-    #    r'bc-size-medium\" >\s*<a class=\"bc-link\s*bc-color-link\" tabindex=\"0\"  href=\"(?P<audiobook_link>.*?)\">'
-    #    r'Release date:(?P<releade_date>.*?)</span>'
-    #    r'bc-color-base\"  >Regular price: </span>\s*<span class=\"bc-text\s*bc-size-base \s*bc-color-base\"  >\s*(?P<price>.*?)\s',
-    #    flags=re.DOTALL
-    #)
-    title_sample = re.compile(
-        r"productListItem\" aria-label='(?P<title>.*?)'>", 
-        flags=re.DOTALL
-    )
-    audiobook_link_sample = re.compile(
-        r'bc-size-medium\" >\s*<a class=\"bc-link\s*bc-color-link\" tabindex=\"0\"  href=\"(?P<audiobook_link>.*?)\">', 
-        flags=re.DOTALL
-    )
-    release_date_sample = re.compile(
-        r'Release date:(?P<releade_date>.*?)</span>', 
-        flags=re.DOTALL
-    )
-    price_sample = re.compile(
-        r'bc-color-base\"  >Regular price: </span>\s*<span class=\"bc-text\s*bc-size-base \s*bc-color-base\"  >\s*(?P<price>.*?)\s', 
-        flags=re.DOTALL
-    )
-    length_sample = re.compile(
-        r'Length:(?P<length>.*?)</span>', #2times per audiobook
+    audiobook_sample = re.compile(
+        r"productListItem\" aria-label='(?P<title>.*?)'>.*?"
+        r'<li class=\"bc-list-item\" >\s*By:\s*(?P<author>.*?)\s*</li>.*?'
+        r'<li class=\"bc-list-item\" >\s*Narrated by:\s*(?P<narrator>.*?)\s*</li>.*?'
+        r'<p  class=\"bc-text\s*bc-spacing-small\s*bc-spacing-top-none\s*bc-size-small\s*bc-color-base\">\s*<p>(?P<summary>.*?)</p>.*?'
+        r'bc-size-medium\" >\s*<a class=\"bc-link\s*bc-color-link\" tabindex=\"0\"  href=\"(?P<audiobook_link>.*?)\">.*?'
+        r'Length:(?P<length>.*?)</span>.*?'
+        r'Release date:(?P<release_date>.*?)</span>.*?'
+        r'bc-color-base\"  >Regular price: </span>\s*<span class=\"bc-text\s*bc-size-base \s*bc-color-base\"  >\s*(?P<price>.*?)\s',
         flags=re.DOTALL
     )
 
     for block in block_sample.finditer(html):
-        #audiobook = audiobook_sample.search(block.group(0)).groupdict()
-        title = title_sample.search(block.group(0))
-        audiobook_link = audiobook_link_sample.search(block.group(0))
-        release_date = release_date_sample.search(block.group(0))
-        price = price_sample.search(block.group(0))
-        length = length_sample.search(block.group(0))
-        #print([length, title, audiobook_link, release_date, price])
-        print(audiobook_link)
+        audiobook = audiobook_sample.search(block.group(0)).groupdict()
+        audiobook['summary'] = audiobook['summary'].replace('<i>','').replace('</i>','').strip()
+        audiobook['release_date'] = audiobook['release_date'].strip()
+        yield audiobook
 
-#nekineki('html_files/first_page.html')
 def save_all_html():
     for n in range(24):
         page_url = find_page_url(n+1)
@@ -142,12 +140,14 @@ def main():
     Returns
     ----------
     '''
-    num_pages = 20
-    df =  pd.DataFrame(data={'Title':[], 'Author':[], 'Narrator':[], 'Release':[], 'Length':[], 'Price':[], 'Rating':[], 'Categories':[]})
-    for html_file in os.listdir('html_files'):
-        html_filename = '...'
-        partial_df = create_partial_df(html_filename)
-        add_to_df(partial_df)
-    return df
-
-save_all_html()
+    #df =  pd.DataFrame(data={'Title':[], 'Author':[], 'Narrator':[], 'Release':[], 'Length':[], 'Price':[], 'Rating':[], 'Categories':[]})
+    audiobooks = []
+    for file_name in os.listdir('html_files'):
+        html_path = os.path.join('html_files', file_name)
+        for audiobook in read_from_html(html_path):
+            audiobooks.append(audiobook)
+    field_names = ['title', 'author', 'narrator', 'summary', 'audiobook_link', 'length', 'release_date', 'price']
+    write_csv(audiobooks, field_names, 'audiobooks.csv')
+#read_from_html('html_files/page_2.html')
+#save_all_html()
+main()
